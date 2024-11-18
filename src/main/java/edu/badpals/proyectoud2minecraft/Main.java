7a0fe6040a7e2382d0a0ce072c7982626106af43
amarinolama <amarinolama@gmail.com>
2024-11-18 14:01:01 +0100

    fix(): arreglar problema anchorPane

--- src/main/java/edu/badpals/proyectoud2minecraft/Main.java
+++ src/main/java/edu/badpals/proyectoud2minecraft/Main.java
@@ -1,12 +1,9 @@
 package edu.badpals.proyectoud2minecraft;
 
-import edu.badpals.proyectoud2minecraft.Controller.MainController;
-import edu.badpals.proyectoud2minecraft.Model.Conexion;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,7 +12,6 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/mainview.fxml"));
-        loader.setRoot(new AnchorPane());
         Parent root = loader.load();
         Scene scene = new Scene(root);
         stage.setScene(scene);
