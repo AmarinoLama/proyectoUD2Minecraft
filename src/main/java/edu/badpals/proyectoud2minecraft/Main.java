dc4c3d42e4d60ef290e5feb420ba88635e0cafb7
Evan <evanchapela@gmail.com>
2024-11-23 16:43:30 +0100

    Style(*.fxml): add styles

--- src/main/java/edu/badpals/proyectoud2minecraft/Main.java
+++ src/main/java/edu/badpals/proyectoud2minecraft/Main.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class Main extends Application {
+public class git agMain extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
